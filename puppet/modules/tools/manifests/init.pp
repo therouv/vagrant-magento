9c9b6e16534d3942f0b0420907f03939a4a3ab80
Rouven Alexander Rieker <therouv@googlemail.com>
2013-12-09 18:04:49 +0100

    Exclude casperjs + installer

--- puppet/modules/tools/manifests/init.pp
+++ puppet/modules/tools/manifests/init.pp
@@ -1,10 +1,10 @@
 class tools {
 
-    include tools::installer
+    #include tools::installer
     include tools::modman
     include tools::composer
     include tools::magerun
-    include tools::casperjs
+    #include tools::casperjs
     include tools::phpunit
 
 }
